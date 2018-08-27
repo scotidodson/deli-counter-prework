19143015ef0bd5507a657b442b9976b1f7d84c54
Scoti Dodson <scotidodson@gmail.com>
2018-08-27 14:11:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,12 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
+  else 
+    katz_deli_line = []
+    katz_deli.each_with_index do |name, index|
+      katz_deli_line << "#{index}. #{name}"
+    end
+    puts "The line is currently #{katz_deli_line}."
   end
 end
   
